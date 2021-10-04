a58962d21e13569ff31067dcf878c4be0b89185f
MatosLarissa <larissamatos88@outlook.com>
2021-10-04 17:00:53 -0300

    Exercícios sobre variáveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,62 @@
+// EXERCICIO 1
+
+
+//Resposta: console.log(10)
+//Resposta: console.log(10,10
+
+// EXERCICIO 2
+
+//Resposta: console.log(10,10,10)
+
+// EXERCICIO 3
+
+// let horasPorDia = prompt(" Quantas horas você trabalha por dia?")
+// let recebePorDia = ("Quanto você recebe por dia?")
+
+// alert(`Voce recebe ${recebePorDia/horasPorDia} por hora`)
+
+// EXERCICIO DE ESCRITA DE CÓDIGO
+
+// const nome = prompt ("Qual o seu nome?")
+// const idade = prompt ("Qual a sua idade?")
+
+// console.log(typeof nomestring)
+// console.log(typeof idadeNumber)
+// //Resposta: string string
+
+// //Resposta: string undefined 
+
+// alert(`Olá ${nome} você tem ${idade} anos`)
+
+// EXERCICIO 4
+
+// const roupa = prompt ("Você esta usando uma roupa branca hoje?")
+// const cabelo = prompt ("O seu cabelo é curto?")
+// const corCabelo = prompt ("A cor do seu cabelo é o preto?")
+
+// //console.log(roupa, cabelo, corCabelo)
+
+// alert(`Você esta usando uma roupa branca hoje? - ${roupa} // O seu cabelo é curto? - ${cabelo} // A cor do seu cabelo é preto? - ${corCabelo}`)
+
+///EXERCICIO 5
+
+// let a = 10
+// let b = 25
+// let c = a
+// a = b
+// b = a
+// b = c
+
+
+// console.log("O novo valor de a é", a)
+// console.log("O novo valor de b é", b)
+// Resposta: O novo valor de a é 25 // O novo valor de b é 10
+
+//Desafio
+
+let bruto = prompt ("Quanto em reais você irá depositar?")
+let liquido = prompt ("Quando você irá retirar?")
+let a = bruto
+let b = liquido
+console.log ("O seu saldo restante agora é de", a-b , "reais")
+alert(`O seu saldo restante agora é de ${a-b} reais`)
