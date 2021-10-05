f6f8fcbd7b438c242b7bc23d4e5e451a6b3677e7
MatosLarissa <larissamatos88@outlook.com>
2021-10-05 16:53:39 -0300

    Atividade sobre Operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,72 @@
+// EXERCICIOS
+
+// Interpretação de código
+
+// 1 - Resposta:
+
+// console.log("a. ", resultado) = false
+ 
+// console.log("b. ", resultado) = false
+
+// console.log("c. ", resultado) = true
+
+// console.log("d. ", typeof resultado) = booleano
+
+
+// 2 - Resposta:
+// O prompt irá retornar uma String então ele precisa especificar que precisa do retorno em Number
+
+
+// 3- Resposta:
+// Exemplo: let soma = Number(primeiroNumero) + Number(segundoNumero)
+
+
+// Interpretação de código
+
+// 1 RESPOSTA:
+// let idade1 = prompt ("Qual a sua idade?")
+// let idade2 = prompt ("Qual a idade so seu amigo?")
+
+// let resultado = Number(idade1) > Number(idade2)
+
+// console.log ("Sua idade é maior que a do seu melhor amigo?", resultado)
+
+// 2 RESPOSTA:
+
+// const numeroPar = prompt ("Informe um número par")
+
+// console.log ("O resto da divisão é", numeroPar % 2)
+// COMENTÁRIO 1: Ao testar com números pares tive o retorno do número 0 
+// COMENTÁRIO 2: Ao testar com números ímpares tive o retorno do número 1 
+
+
+// 3 RESPOSTA:
+// const idade = prompt ("Qual a sua idade?")
+
+// console.log ("Sua idade em meses", idade * 12 ) 
+// console.log ("Sua idade em dias", idade * 365 ) 
+// console.log ("Sua idade em horas", idade * 365 * 24) 
+
+//4 RESPOSTA:
+
+// const primeiroNumero = prompt ("Primeiro número")
+// const segundoNumero = prompt ("Segundo número")
+
+
+// // O primeiro numero é maior que segundo? true
+// console.log (Number(primeiroNumero) > Number(segundoNumero))
+
+// // O primeiro numero é igual ao segundo? false
+// console.log (Number(primeiroNumero) === Number(segundoNumero))
+
+// // O primeiro numero é divisível pelo segundo? true
+// const resto = Number(primeiroNumero) % Number (segundoNumero)
+// console.log (resto === 0)
+
+// // O segundo numero é divisível pelo primeiro? true
+// const resto2 = Number(segundoNumero) % Number (primeiroNumero)
+// console.log (resto2 === 0)
+
+
+
+
