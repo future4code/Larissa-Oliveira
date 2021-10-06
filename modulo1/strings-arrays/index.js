c1ade49223991fb81a63841c4c699ead72fee7a7
MatosLarissa <larissamatos88@outlook.com>
2021-10-06 18:30:35 -0300

    Exercicio Strigs-Arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,57 @@
+// Exercícios de interpretação de código
+
+// 1.  Indique todas as mensagens impressas no console, **SEM EXECUTAR o programa**.
+// a) undefined
+// b) null
+// c) 11
+// d) 0
+// e) 20
+// f) 26
+
+// 2. Leia o código abaixo com atenção 
+// Resposta SUBI NUM ÔNIBUS EM MIRROCOS // 27
+
+// - **Exercícios de escrita de código**
+// 1 - Pedir nome e e-mail do usuário
+// const nomeDoUsuario = prompt("Qual o seu nome?")
+// const emailDoUsuario = prompt("Qual o seu e-mail?")
+// const mensagem = `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`
+// console.log(mensagem)
+
+// 2 - Fazer um programa que contenha um array com 5 comidas preferidas, armazenado em  variavel.
+// const comidasFavoritas = ["Sorvete", "Pizza", "Sanduiche", "Hamburguer", "Calzone"]
+// const comidadoUsuario = prompt ("Qual a sua comida favorita?")
+// comidasFavoritas[2] = comidadoUsuario
+// console.log(`Essas são as minhas comidas preferidas 
+//            ${comidasFavoritas[0]}
+//            ${comidasFavoritas[1]}
+//            ${comidasFavoritas[2]}
+//            ${comidasFavoritas[3]}
+//            ${comidasFavoritas[4]}`)
+
+           
+// // 3 - 3. Faça um programa, seguindo os passos:
+    
+//     // a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+//     let listaDeTarefas = []
+    
+//     // b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+//     let tarefaUM = prompt ("Diga a primeira tarefas que precisa realizar")
+//     let tarefaDois = prompt ("Diga a segunda tarefas que precisa realizar")
+//     let tarefaTres = prompt ("Diga a terceira tarefas que precisa realizar")
+
+//     listaDeTarefas.push(tarefaUM, tarefaDois, tarefaTres)
+
+//     // c) Imprima o array no console
+//     console.log (listaDeTarefas)
+    
+//     // d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2 
+//     const tarefaRealizada = prompt ("Escolha entre 0, 1, 2 qual trafera você já realizou?")
+    
+
+// //     // e) Remova da lista o item de índice que o usuário escolheu.
+//     const resultado = listaDeTarefas.splice(tarefaRealizada, 1)
+
+// //    // f) Imprima o array no console
+//     console.log (listaDeTarefas)
+
