732326fd1e9807a585e52e933d6c562dffd5a4e3
MatosLarissa <larissamatos88@outlook.com>
2022-01-18 18:45:14 -0300

    Exercícios Backend

---
+++ modulo4/Atividades/node-package-json/Exercício-1/src/index.js
@@ -0,0 +1,16 @@
+// a) como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+// Resposta: process.argv[i]
+
+// b)Crie um programa que receba seu nome e sua idade.
+//  Após receber estes valores,
+//  imprima no console uma mensagem que siga a seguinte estrutura:
+// Resposta: 
+// const indexName = process.argv[2];
+// const indexAge = Number(process.argv[3]);
+// console.log(`Olá, ${indexName}! você tem ${indexAge} anos.`)
+
+// c)mostre também a sua idade daqui a sete anos.
+// Resposta: 
+// const indexName = process.argv[2];
+// const indexAge = Number(process.argv[3]);
+// console.log(`Olá, ${indexName}! você tem ${indexAge} anos.  Em sete anos você terá ${indexAge + 7}`)
---
+++ modulo4/Atividades/node-package-json/Exercício-3/src/index.js
@@ -0,0 +1,18 @@
+// Crie uma aplicação Node que receba uma string representando **uma tarefa.** 
+// O programa deve adicionar a **nova tarefa** 
+// em uma variável que represente uma **lista de tarefas.** 
+// A lista de tarefas pode estar criada antes da execução do código. 
+// Após adicionar o item à lista, exiba a lista atualizada.
+
+// const tarefa = "arrumar a cama"
+
+// const listaDeTarefa = ["lavar a mão", "comer"]
+
+// const novaTarefa = process.argv[2]
+
+// const novaLista = [...listaDeTarefa, tarefa ]
+
+// // novaLista.push(novaTarefa)
+
+// console.log(novaLista)
+
