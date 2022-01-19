b5344c8099bbcc62c6190bbf0e80b30a6e62b21a
MatosLarissa <larissamatos88@outlook.com>
2022-01-19 16:48:14 -0300

    Exercícios

---
+++ modulo4/Atividades/Introdução a Typescript/First-script/Exercício-2/src/index.ts
@@ -0,0 +1,6 @@
+function imprimeTresCoresFavoritas() {
+    const cor1: string = String(prompt("Insira sua primeira cor favorita"))
+    const cor2: string = String(prompt("Insira sua segunda cor favorita"))
+    const cor3: string = String(prompt("Insira sua terceira cor favorita"))
+    console.log([cor1, cor2, cor3])
+ }
